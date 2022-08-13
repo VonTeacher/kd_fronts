44fd836e5ffdd420d8981c1a10d61881f727b22d
Vaughn Weiss <vaughn.weiss@gmail.com>
2022-08-13 15:05:46 -0600

    Add ApolloClient and display some objects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_ALL_ARTISTS = gql`
+  query GetAllArtists {
+    artists {
+      id
+      name
+    }
+  }
+`;
+
+const GET_ALL_ALBUMS = gql`
+  query GetAllAlbums {
+    albums {
+      id
+      title
+      artist
+    }
+  }
+`;
+
+function DisplayAllArtists() {
+  const { loading, error, data } = useQuery(GET_ALL_ARTISTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.artists.map(({ id, name }) => {
+    return (
+      <div key={id}>
+        <h2>{name}</h2>
+      </div>
+    )
+  })
+}
+
+function DisplayAllAlbums() {
+  const { loading, error, data } = useQuery(GET_ALL_ALBUMS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.log(error)
+  };
+
+  return data.albums.map(({ id, title, artist }) => {
+    return (
+      <div key={id}>
+        <div>{title} by {artist}</div>
+      </div>
+    )
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Kate's Collection: Artists</h1>
+      <br />
+      <DisplayAllArtists />
+      <br />
+      <h1>Kate's Collection: Albums</h1>
+      <br />
+      <DisplayAllAlbums />
     </div>
   );
 }
