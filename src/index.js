a4f10cb82d38a2754df51bfc7fe185f3dcba8c8f
Vaughn Weiss <17757205+VonTeacher@users.noreply.github.com>
2022-08-13 15:26:07 -0600

    Apollo graphql (#1)
    * Add ApolloClient and display some objects
    * Remove personal name
    * Use env variables for client URI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const clientUri = () => {
+  return (!process.env.NODE_ENV  || process.env.NODE_ENV === "development")
+    ? process.env.REACT_APP_DEVELOPMENT_URI
+    : process.env.REACT_APP_PRODUCTION_URI
+}
+
+const client = new ApolloClient({
+  uri: clientUri(),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
