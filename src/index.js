d01e8c8783407a6f284ef2e8cbe9e0af9820855d
Vaughn Weiss <17757205+VonTeacher@users.noreply.github.com>
2022-08-13 21:49:09 -0600

    Add some console garbage (#3)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider
+  ApolloProvider,
+  gql
 } from '@apollo/client';
 import App from './App';
 
 const clientUri = () => {
+  console.log(process.env.NODE_ENV)
+  console.log(process.env.REACT_APP_PRODUCTION_URI)
   return (!process.env.NODE_ENV  || process.env.NODE_ENV === "development")
     ? process.env.REACT_APP_DEVELOPMENT_URI
     : process.env.REACT_APP_PRODUCTION_URI
@@ -17,6 +20,19 @@ const client = new ApolloClient({
   uri: clientUri(),
   cache: new InMemoryCache(),
 });
+
+client
+  .query({
+    query: gql`
+      query GetAllAlbums {
+        albums {
+          title
+          user
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
